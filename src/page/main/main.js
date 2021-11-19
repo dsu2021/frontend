6b22d6fe55debff3a6620ebad868b6bb976d9432
배성원 <baeseong-won@baeseong-won-ui-MacBook-Pro.local>
2021-11-19 13:47:48 +0900

    라우터 및 기본 뼈대 구조 개발

---
+++ src/page/main/main.js
@@ -0,0 +1,18 @@
+import './main.scss';
+
+function Main() {
+    return (
+        <section className='main-content'>
+            <div className='flexBox'>
+                <div className='item'>content1</div>
+                <div className='item'>content2</div>
+                <div className='item'>content3</div>
+                <div className='item'>content4</div>
+                <div className='item'>content5</div>
+                <div className='item'>content6</div>
+            </div>
+        </section>
+    )
+}
+
+export default Main;
